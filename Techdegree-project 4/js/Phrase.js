e5bc80a4e3a67f56f77fada9115dbfaa3befbf9b
Myles Y <39877233+mylesz123@users.noreply.github.com>
2018-11-20 15:43:38 -0500

    am i heading in the right direction?

--- Techdegree-project 4/js/Phrase.js
+++ Techdegree-project 4/js/Phrase.js
@@ -8,13 +8,18 @@ class Phrase {
   addPhraseToDisplay(){
     //this method adds letter placeholders to the display when the game starts.
     //Each letter is presented by an empty box, one list item for each letter.
+    const letter = /^[a-zA-Z]+$/;
+    const space = /^\s+$/;
+    let hideLetter = `<li class="hide letter"> </li>`;
+    let hideSpace = `<li class="hide space"> </li>`;
+    if(phrase === letter){//phrase.value.match(letter);
+      //add the kind of list item needed for a letter
+      document.createElement(hideLetter);
+    }
+    else if(phrase === space){
+      //add list item needed for a space.
+      document.createElement(hideLetter);
+    }
   }
 
 }//end phrase class
-
-$('#btn__reset').on('click', ()=>{
-  //when start game is clicked, addPhraseToDisplay method should begin.
-  let showPhrase = new Phrase();
-  showPhrase.forEach(phrase => addPhraseToDisplay;
-
-});//end click event
--- Techdegree-project 4/js/app.js
+++ Techdegree-project 4/js/app.js
@@ -1,2 +1,8 @@
 console.log('hi');
 //perform basic DOM selection, add event handlers, and to reset the game when it ends
+$('#btn__reset').on('click', ()=>{
+  //when start game is clicked, addPhraseToDisplay method should begin.
+  let showPhrase = new Phrase();
+  showPhrase.forEach(phrase => addPhraseToDisplay;
+
+});//end click event
